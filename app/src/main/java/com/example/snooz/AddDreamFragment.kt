7331392823a72a3e344e771ab43a09a656c2e40f
Ara <oladipoeyiara@gmail.com>
2023-02-27 14:34:39 -0500

    Pushing dream data to firebase, and updating styles of registration flow

--- app/src/main/java/com/example/snooz/AddDreamFragment.kt
+++ app/src/main/java/com/example/snooz/AddDreamFragment.kt
@@ -5,14 +5,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.example.snooz.databinding.FragmentAddDreamBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
+class Dream{
+    var name = ""
+    var tag = ""
+    var text = ""
+    constructor(name: String, tag: String, text: String){
+        this.name = name
+        this.tag= tag
+        this.text = text
+    }
+}
 
 class AddDreamFragment : Fragment() {
+    private var _binding: FragmentAddDreamBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var database: DatabaseReference
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_dream, container, false)
+        _binding = FragmentAddDreamBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        database = Firebase.database.reference
+
+        //Get users current id
+        val storedData = activity?.getSharedPreferences("sharedPreferences", AppCompatActivity.MODE_PRIVATE)
+        val sharedPreferencesId = storedData?.getString("id", "")
+
+        //once the user clicks the button, push the data to firebase
+        _binding!!.addDreamButton.setOnClickListener {
+            val currentDreamData = Dream(binding.dreamNameInput.text.toString(), binding.dreamTagInput.text.toString(), binding.dreamTextInput.text.toString());
+            database.child("/${sharedPreferencesId}/dreams").push().setValue(currentDreamData)
+        }
+        return view
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/example/snooz/MainActivity.kt
+++ app/src/main/java/com/example/snooz/MainActivity.kt
@@ -21,67 +21,73 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
+        try {
+            super.onCreate(savedInstanceState)
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            val view = binding.root
 
-        setContentView(view)
+            setContentView(view)
 
-        //Initilize authentication
-        auth = Firebase.auth
-        val currentUser = auth.currentUser
+            //Initilize authentication
+            auth = Firebase.auth
+            val currentUser = auth.currentUser
 
 
-        Log.d("w", "here's the main activity")
-        val storedData = getSharedPreferences("sharedPreferences", MODE_PRIVATE)
-        val sharedPreferencesId = storedData.getString("id", "")
-        Log.d("eeeewewfwefw", sharedPreferencesId.toString())
+            Log.d("w", "here's the main activity")
+            val storedData = getSharedPreferences("sharedPreferences", MODE_PRIVATE)
+            val sharedPreferencesId = storedData.getString("id", "")
+            Log.d("eeeewewfwefw", sharedPreferencesId.toString())
 
-        //Add token listener to check if user signs out
-        auth.addIdTokenListener(FirebaseAuth.IdTokenListener {
-            if(it.currentUser?.uid == null){
-                //Remove user id from shared preferences
-                val sharedPreference = getSharedPreferences("sharedPreferences", MODE_PRIVATE).edit()
-                sharedPreference.remove("id")
-                sharedPreference.apply()
+            //Add token listener to check if user signs out
+            auth.addIdTokenListener(FirebaseAuth.IdTokenListener {
+                if (it.currentUser?.uid == null) {
+                    //Remove user id from shared preferences
+                    val sharedPreference =
+                        getSharedPreferences("sharedPreferences", MODE_PRIVATE).edit()
+                    sharedPreference.remove("id")
+                    sharedPreference.apply()
 
-                //Route back to signup page
-                val i = Intent(this, Signup::class.java)
-                startActivity(i)
-            }else{
-                Log.d("eee", currentUser.toString())
-            }
-        })
+                    //Route back to signup page
+                    val i = Intent(this, Signup::class.java)
+                    startActivity(i)
+                } else {
+                    Log.d("eee", currentUser.toString())
+                }
+            })
 //
-        binding.logOutButton.setOnClickListener{
-            val edit = getSharedPreferences("sharedPreferences", MODE_PRIVATE).edit()
-            edit.remove("id")
-            edit.apply()
-            Firebase.auth.signOut()
+            binding.logOutButton.setOnClickListener {
+                val edit = getSharedPreferences("sharedPreferences", MODE_PRIVATE).edit()
+                edit.remove("id")
+                edit.apply()
+                Firebase.auth.signOut()
 
-            Log.d("wewew", auth.currentUser.toString())
+                Log.d("wewew", auth.currentUser.toString())
 
-        }
-
-        binding.addDreamButton.setOnClickListener {
-            supportFragmentManager.commit {
-                replace<AddDreamFragment>(R.id.fragmentContainerView)
-                setReorderingAllowed(true)
-                addToBackStack("name") // name can be null
             }
-        }
 
-        binding.viewAllDreamsButton.setOnClickListener {
+            binding.addDreamButton.setOnClickListener {
+                supportFragmentManager.commit {
+                    replace<AddDreamFragment>(R.id.fragmentContainerView)
+                    setReorderingAllowed(true)
+                    addToBackStack("name") // name can be null
+                }
+            }
+
+            binding.viewAllDreamsButton.setOnClickListener {
 //            val fragmentManager: FragmentManager = supportFragmentManager
 //            fragmentManager.beginTransaction().replace(R.id.fragmentContainerView, AddDreamFragment::class.java, null)
 //                .setReorderingAllowed(true)
 //                .addToBackStack("name").commit()
-            supportFragmentManager.commit {
-                replace<ViewAllDreamsFragment>(R.id.fragmentContainerView)
-                setReorderingAllowed(true)
-                addToBackStack("name") // name can be null
-            }
+                supportFragmentManager.commit {
+                    replace<ViewAllDreamsFragment>(R.id.fragmentContainerView)
+                    setReorderingAllowed(true)
+                    addToBackStack("name") // name can be null
+                }
 
+            }
+        }catch (e: Exception) {
+            Log.e("AAAAAAAAA", "onCreateView", e);
+            throw e;
         }
    }
 }
